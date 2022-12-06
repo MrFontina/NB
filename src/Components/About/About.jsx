f14c18b34ad9cb062a0563fc830483e4bd0166ef
Nahuel Bracchitta <nahuelbracchitta@gmail.com>
2022-12-06 15:59:55 -0300

    empieza a andar

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,16 +1,26 @@
-
+import "./about.css"
+import image from "./perfil.jpg"
 
 function About() {
-  return (
-    <div className="container.fluid">
-        <div className="row">
 
-            <div className="col-8"  style={{backgroundColor:"pink"}}>
-            caca
+  
+
+  return (
+    <div className="hector">
+    <div className="container.fluid p-5 ">
+        <div className="row flex-wrap" >
+
+            <div className="col-lg-7 col-12"  style={{backgroundColor:"pink", textAlign:"center", paddingTop:"100px", height:"500px"}}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni deserunt dolor facere quam quis reprehenderit corrupti. Impedit architecto quis ducimus ad est non cum harum, sit, molestiae tempora itaque at. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut suscipit voluptates fuga quo dolores consequatur dicta quos, rerum voluptatum saepe molestiae modi ipsa illum eaque non assumenda similique excepturi nobis! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia, nostrum reiciendis. Corrupti harum culpa eos. Ut aperiam repellendus esse dolore fugiat laborum nihil in, quo, cupiditate dicta repellat illum distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia autem, temporibus aperiam, dolore ullam dolorem quo suscipit nostrum nobis ab eius saepe pariatur tempora a rerum, cupiditate facere laborum dolorum!
         </div>
-        <div className="col-4" style={{backgroundColor:"green"}}>HOLA</div>
+        <div className="rogelio col-lg-5 col-12 "  >
+            <img src={image} style={{borderRadius: "50%", height: "50%" }} alt=""/>
+            {/* <img src={image} style={{borderRadius: "50%", height: "50%" }} alt=""/>
+            <img src={image} style={{borderRadius: "50%", height: "50%" }} alt=""/> */}
+            </div>
         </div>
         
+    </div>
     </div>
   )
 }
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -6,30 +6,25 @@ function NavBar() {
 
 
   return (
-    <nav className="navbar navbar-expand-lg pt-0" style={{backgroundColor:"#ffffff"}}>
-  <div className="container-fluid" style={{backgroundColor:"red"}}>
-    <Link className="navbar-brand mx-5 mt-3 mb-3" to="/">Nahuel Bracchitta</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    
+    
+<nav className="navbar navbar-expand-lg bg-light ">
+  <div className="container-fluid">
+    <Link className="navbar-brand">Navbar</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarText">
-      <ul className="navbar-nav ">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" >Home</Link>
-        </li>
-        <li className="nav-item">
+    <div className="collapse navbar-collapse " id="navbarNavAltMarkup">
+      <div className="navbar-nav">
         <Link className="nav-link active" aria-current="page" >Home</Link>
-        </li>
-        <li className="nav-item">
-        <Link className="nav-link active" aria-current="page" >Home</Link>
-        </li>
-        {/* <li className="nav-item d-flex align-items-center">
-          <span >🌙</span>
-        </li> */}
-      </ul>
+        <Link className="nav-link" >Features</Link>
+        <Link className="nav-link" >Pricing</Link>
+        {/* <Link className="nav-link disabled">Disabled</Link> */}
+      </div>
     </div>
   </div>
 </nav>
+
   )
 }
 
