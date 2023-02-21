808989ac1fa395f5e4859d8f6bc272174b7e039f
Nahuel Bracchitta <nahuelbracchitta@gmail.com>
2023-02-21 09:35:14 -0300

    proyectos actualizado

--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -2,10 +2,10 @@ import Card from "../Card/Card"
 import { useContext } from "react"
 import themeContext from "../../Storage/ThemeContext"
 import gif1 from "./musicstore.gif"
-import gif2 from "./juan.gif"
+// import gif2 from "./juan.gif"
 import gif3 from "./nbgif.gif"
 import gif4 from "./gif4.gif"
-import gif5 from "./gif5.gif"
+// import gif5 from "./gif5.gif"
 import gif6 from "./gif6.gif"
 import "./proyectos.css"
 import image4 from "./e1.png"
@@ -31,43 +31,41 @@ function Proyectos() {
                               <Card title="React Ecommerce"
                                     img={image4}
                                     poptitle="React Commerce"
-                                    about="Proyecto final para el curso de React JS. El objeto fue hacer un Ecommerce, con un carrito totalmente funcional, y que se genere una orden de compra que se envie a una base de datos (Firebase)."
+                                    about="Simulación de un Ecommerce de una tienda de instrumentos. Tiene un carrito totalmente funcional, que al terminar la compra envía una orden a una base de datos en Firebase. Se hizo completamente en React."
                                     gif={gif1}
                                     link="https://reactecommercebracchitta.netlify.app/"
                                     git="https://github.com/MrFontina/reactBracchitta"
                               />
 
-                              <Card title="WindBnb"
-                                    img={image6}
-                                    poptitle="WindBnb"
-                                    about="Sitio en proceso estilo AirBnb. Copiado a partir de un diseño de Figma."
-                                    gif={gif6}
-                                    link="https://nbwindbnb.netlify.app/"
-                                    git="https://github.com/MrFontina/windbnb"
-                              />
-
-
                               <Card title="Music Biography"
-                                    img={image5}
+                                    img={image6}
                                     poptitle="Music Biography"
-                                    about="Proyecto final para el curso de desarrollo web. Se utilizó HTML5, CSS3, Bootstrap y SASS. Es un sitio totalmente responsive."
+                                    about="Sitio totalmente responsive. Se utilizó HTML5, CSS3, Bootstrap y SASS."
                                     gif={gif3}
                                     link="https://nahuelbracchitta.netlify.app/index.html"
                                     git="https://github.com/MrFontina/proyectofinal"
                               />
 
                               <Card title="Movies API"
-                                    img={image6}
+                                    img={image5}
                                     poptitle="Movies API"
-                                    about="Desarrollo de sitio de peliculas como parte del bootcamp de Alkemy, lee un listado de una api, tiene un buscador y una seccion de agregar a favoritos."
+                                    about="Desarrollo de sitio de peliculas como parte del bootcamp de Alkemy, lee un listado de una api, tiene un buscador y una seccion de agregar a favoritos. Desarrollado en React"
                                     gif={gif4}
                                     link="https://alkemymoviesnb.netlify.app/"
                                     git="https://github.com/MrFontina/AlkemyMovies2"
                               />
 
+                              <Card title="WindBnb"
+                                    img={image6}
+                                    poptitle="WindBnb"
+                                    about="SITIO EN PROCESO. Estilo AirBnb. Copiado a partir de un diseño de Figma. Desarrollado en React"
+                                    gif={gif6}
+                                    link="https://nbwindbnb.netlify.app/"
+                                    git="https://github.com/MrFontina/windbnb"
+                              />
 
 
-                              <Card title="JS Ecommerce"
+                              {/* <Card title="JS Ecommerce"
                                     img={image6}
                                     poptitle="JS Ecommerce"
                                     about="Proyecto final para el curso de JavaScript. No se tuvo en cuenta los estilos. No es responsive."
@@ -86,7 +84,7 @@ function Proyectos() {
                                     gif={gif5}
                                     link="https://nbdrumkitjs.netlify.app/"
                                     git="https://github.com/MrFontina/basketballscore"
-                              />
+                              /> */}
 
 
                         </div>
